f4a549edc2bddb1e37f3bfbda2db708ab3486be9
MFMFloppY <tkomarov098@gmail.com>
2025-05-15 16:21:31 +0300

    added db and cache values to settings file

---
+++ settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: int = 5432
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "password"
+    DB_NAME: str = "pomodoro"
+    CACHE_HOST: str = "0.0.0.0"
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
